13e3999da45c25beef9a85927ca2ae95209f5ff1
sophisticated-snake <jamie.thompson.1024@gmail.com>
2022-01-30 22:52:44 +1300

    style

--- src/components/GameListing.tsx
+++ src/components/GameListing.tsx
@@ -6,7 +6,7 @@ function GameListing(props: { game: Game, onClick: () => void })
 {
     return (
         <div className="GameListing" onClick={props.onClick}>
-            <h2>{props.game.name}</h2>
+            <h2 className="GameListing-Header">{props.game.name}</h2>
             <div className="GameListing-Scores">
                 <p>{props.game.topScore}</p>
                 <p>{props.game.averageScore}</p>
